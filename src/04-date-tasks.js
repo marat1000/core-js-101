eb9acc9976443a04330b7752c3e1fca028ad93b6
unknown <marat76593@gmail.com>
2022-07-25 01:56:31 +0300

    feat: implement objects tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return Date.parse(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,14 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  let res = false;
+  if ((year % 4 === 0 && year % 100 !== 0)
+    || (year % 4 === 0 && year % 100 === 0 && year % 400 === 0)) {
+    res = true;
+  }
+  return res;
 }
 
 
@@ -73,8 +79,12 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const hours = String(endDate.getHours() - startDate.getHours());
+  const mins = String(endDate.getMinutes() - startDate.getMinutes());
+  const secs = String(endDate.getSeconds() - startDate.getSeconds());
+  const mss = String(endDate.getMilliseconds() - startDate.getMilliseconds());
+  return `${hours.padStart(2, 0)}:${mins.padStart(2, 0)}:${secs.padStart(2, 0)}.${mss.padStart(3, 0)}`;
 }
 
 
@@ -94,8 +104,18 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  let h = date.getUTCHours();
+  let m = date.getUTCMinutes();
+  if (h > 11) h -= 12;
+  if (m === 60) {
+    m = 0;
+  }
+  const hourAngle = 0.5 * (h * 60 + m);
+  const minuteAngle = 6 * m;
+  let angle = Math.abs(hourAngle - minuteAngle);
+  angle = Math.min(360 - angle, angle);
+  return (angle * Math.PI) / 180;
 }
 
 
--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,8 +20,10 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+  this.getArea = () => width * height;
 }
 
 
@@ -35,8 +37,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 
@@ -51,8 +53,10 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const res = JSON.parse(json);
+  Object.setPrototypeOf(res, proto);
+  return res;
 }
 
 
@@ -110,33 +114,109 @@ function fromJSON(/* proto, json */) {
  *  For more examples see unit tests.
  */
 
+function wrongOrder() {
+  throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+}
+
+function noMulti() {
+  throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+}
+
+class Selector {
+  constructor() {
+    this.currentNumber = 0;
+    this.hasElementName = false;
+    this.hasID = false;
+    this.hasPseudoElement = false;
+    this.units = [];
+  }
+
+  element(value) {
+    if (this.currentNumber > 0) wrongOrder();
+    if (this.hasElementName === true) noMulti();
+    this.currentNumber = 0;
+    this.hasElementName = true;
+    this.units.push(`${value}`);
+    return this;
+  }
+
+  id(value) {
+    if (this.currentNumber > 1) wrongOrder();
+    if (this.hasID === true) noMulti();
+    this.currentNumber = 1;
+    this.hasID = true;
+    this.units.push(`#${value}`);
+    return this;
+  }
+
+  class(value) {
+    if (this.currentNumber > 2) wrongOrder();
+    this.currentNumber = 2;
+    this.units.push(`.${value}`);
+    return this;
+  }
+
+  attr(value) {
+    if (this.currentNumber > 3) wrongOrder();
+    this.currentNumber = 3;
+    this.units.push(`[${value}]`);
+    return this;
+  }
+
+  pseudoClass(value) {
+    if (this.currentNumber > 4) wrongOrder();
+    this.currentNumber = 4;
+    this.units.push(`:${value}`);
+    return this;
+  }
+
+  pseudoElement(value) {
+    if (this.currentNumber > 5) wrongOrder();
+    if (this.hasPseudoElement === true) noMulti();
+    this.currentNumber = 5;
+    this.hasPseudoElement = true;
+    this.units.push(`::${value}`);
+    return this;
+  }
+
+  stringify() {
+    return this.units.join('');
+  }
+}
+
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  element(value) {
+    return new Selector().element(value);
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    return new Selector().id(value);
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    return new Selector().class(value);
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    return new Selector().attr(value);
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    return new Selector().pseudoClass(value);
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    return new Selector().pseudoElement(value);
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  stringify() {
+    return this.stringify();
+  },
+
+  combine(selector1, combinator, selector2) {
+    const res = new Selector();
+    res.units = [...selector1.units, ` ${combinator} `, ...selector2.units];
+    return res;
   },
 };
 
